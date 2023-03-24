d915b781ea6de14a29cd196b6863ee4031ed1199
C. Kim <charliehkilo@gmail.com>
2023-03-24 12:55:30 +0900

    Create CompactVersion
    Main.java의 간결한 code로 converting.

---
+++ AS0322/src/CompactVersion.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CompactVersion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+
+            int floor = N % H;
+            int roomNumber = (N / H) + 1;
+
+            if (floor == 0) {
+                floor = H;
+                roomNumber--;
+            }
+
+            System.out.println(floor * 100 + roomNumber);
+        }
+    }
+}
