71b63128e73c96cda19d5624e96d8d3dc8970311
C. Kim <charliehkilo@gmail.com>
2023-03-29 22:47:21 +0900

    Update AS0329
    문제 해결 완료.
    문제 번호 : 2775
    문제 이름 : 부녀회장이 될테야
    https://www.acmicpc.net/problem/2775

--- AS0329/src/Main.java
+++ AS0329/src/Main.java
@@ -10,29 +10,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Test case의 개수를 입력하시오. : ");
+//        System.out.print("Test case의 개수를 입력하시오. : ");
         int testCaseCounter = scanner.nextInt();
 
         for (int i = 0; i < testCaseCounter; i++) {
-            System.out.print("몇 층인지 입력하시오. : ");
+//            System.out.print("몇 층인지 입력하시오. : ");
             int k = scanner.nextInt();
 
-            System.out.print("몇 호인지 입력하시오. : ");
+//            System.out.print("몇 호인지 입력하시오. : ");
             int n = scanner.nextInt();
 
             int peopleCounter = 0;
 
-            for (int j = 0; j <= k; j++) {
-                for (int l = 1; l <= n; l++) {
-                    if (j == 0) {
-                        peopleCounter = j + 1;
+            int[][] rooms = new int[(k + 1)][n];
+            int result = 0;
+
+            for (int a = k; a >= 0; a--) {
+//                System.out.println(a);
+                for (int b = 0; b < n; b++) {
+//                    System.out.print(a + "행 " + b + "열 ");
+                    if (a == k || b == 0) {
+                        rooms[a][b] = b + 1;
                     } else {
-                        peopleCounter = l * j + (l - 1) * j;
+                        rooms[a][b] = rooms[a][b - 1] + rooms[a + 1][b];
+                        result = rooms[a][b];
                     }
+//                    System.out.print(rooms[a][b]);
                 }
+//                System.out.println();
             }
 
-            System.out.println(peopleCounter);
+            System.out.println(result);
+
+//            for (int a = 0; a <= k; a++) {
+//                for (int b = 1; b <= n; b++) {
+////                    System.out.print(a + "층 " + b + "호 ");
+//                    System.out.print(rooms[a][b - 1]);
+//                }
+////                System.out.println();
+//            }
         }
     }
 }
